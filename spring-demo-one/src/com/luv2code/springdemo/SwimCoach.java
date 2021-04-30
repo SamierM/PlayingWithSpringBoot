245f63716cc2948f78942aee95ede6ea473fd5ca
SamierMahagna <samiermahagna57@gmail.com>
2021-04-30 08:46:11 -0700

    Moved properties to a properties file and reference that file

--- spring-demo-one/src/com/luv2code/springdemo/SwimCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/SwimCoach.java
@@ -3,12 +3,43 @@ package com.luv2code.springdemo;
 public class SwimCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
 	
 	public SwimCoach() {
 		System.out.println("We are inside the no arg constructer of our SwimCoach");
 	}
 	
 	
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+
+
+	public void setEmailAddress(String emailAddress) {
+
+		System.out.println("SwimCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+
+
+	public String getTeam() {
+		return team;
+	}
+
+
+
+	public void setTeam(String team) {
+
+		System.out.println("SwimCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+
+
+
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("SwimCoach: inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
